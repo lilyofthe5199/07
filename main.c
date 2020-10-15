561b7662bbe9934d535ad0ceabd01f7a6478ce0e
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-10-15 15:46:54 +0900

    02

--- main.c
+++ main.c
@@ -3,15 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	int flag = 1;
-	
-	while(flag != 0)
+	int i;
+
+	for (i=0;i<5;i++) // 5번 반복 
 	{
-	int y; // y의 범위: while 블록 
-	y = 3;
-	flag = 0;
+	int temp=1;
+	printf("temp = %d\n", temp);
+	temp++;
 	}
+	
 	return 0;
 }
