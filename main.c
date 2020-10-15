02ad3034c4c6138b805a103e00a7f80a9e707b2e
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-10-15 15:53:51 +0900

    03

--- main.c
+++ main.c
@@ -3,16 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter)
+{
+	counter++;
+	return counter;
+}
+
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i=0;i<5;i++) // 5번 반복 
-	{
-	int temp=1;
-	printf("temp = %d\n", temp);
-	temp++;
-	}
+	int i=0;
+	
+	printf("함수 호출전 i=%d\n", i); // i=0
+	inc(i);
+	printf("함수 호출후 i=%d\n", i); // i=0
 	
 	return 0;
 }
+
