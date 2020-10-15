6c1230ec6541ffd2a8fb20dc6accde549e6da6a1
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-10-15 15:33:25 +0900

    01

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void)
+{
+	int flag = 1;
+	
+	while(flag != 0)
+	{
+	int y; // y의 범위: while 블록 
+	y = 3;
+	flag = 0;
+	}
+	return 0;
+}
